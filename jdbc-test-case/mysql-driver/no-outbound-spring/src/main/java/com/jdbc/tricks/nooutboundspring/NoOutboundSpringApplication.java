0153a63a4681ef444d7c9caf7b5de5025422ccde
yulate <yulate531@gmail.com>
2025-04-21 12:13:07 +0800

    fix: 修改no-outbound-spring项目结构

---
+++ jdbc-test-case/mysql-driver/no-outbound-spring/src/main/java/com/jdbc/tricks/nooutboundspring/NoOutboundSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jdbc.tricks.nooutboundspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoOutboundSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoOutboundSpringApplication.class, args);
+    }
+
+}
---
+++ jdbc-test-case/mysql-driver/no-outbound-spring/src/test/java/com/jdbc/tricks/nooutboundspring/NoOutboundSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbc.tricks.nooutboundspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoOutboundSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
